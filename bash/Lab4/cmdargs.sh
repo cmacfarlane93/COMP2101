97a725aa836168994758884c1fab17d8d556ce29
cmacfarlane93 <43827729+cmacfarlane93@users.noreply.github.com>
2020-07-07 05:41:40 -0400

    Update cmdargs.sh
    corrections

--- bash/Lab4/cmdargs.sh
+++ bash/Lab4/cmdargs.sh
@@ -32,15 +32,12 @@ while [ $# -gt 0 ]; do
   	#output verbose enabled
   	echo "verbose mode: "$verbose
   	#excluding -v from being processed
-  	myargs+=("$2")
   	shift
   	;;
   	
   *)	#verbose option not found
   	#output verbose mode not enabled
   	echo "verbose mode: "$verbose
-  	# add whatever is in $1 to the myargs array
-  	myargs+=("$1")
   	;; 
   esac
   
@@ -55,6 +52,50 @@ while [ $# -gt 0 ]; do
   	debugLevel=$2
   	echo "debug level: "$debugLevel
   	#excluding -d and N from being processed
+  	echo "Added \'$3\' to the arguments array"
+  	#shifting twice to exclude both -d and N
+  	shift
+  	shift
+  	;;
+  	
+  *)	#verbose option not found
+  	#output verbose mode not enabled
+  	echo "debug mode: "$debug
+  	# tell the user how many things are left on the command line
+  	echo "There are $# things left to process on the command line."
+  	# tell the user what we did
+  	echo "Added \'$1\' to the arguments array"
+  	;; 
+  	esac
+  	
+  #adding non-optioned input to array		
+  myargs+=("$1") 
+# TASK 1: instead of just adding arguments to an array, use a case statement to recognize some options
+#          Options to recognize: -h for help, -v for verbose mode, -d N for debug mode with N being a single digit number
+#          If the help option is recognized, print out help for the command and exit
+#          If the verbose option is recognized, set a variable to indicate verbose mode is on
+#          If the debug optionis recognized, set a variable with the debug level from the number given after the -d on the command line
+#             display an error if the user gave the -d option without a single digit number after it
+#          Anything that wasn't recognized on the command line should still go into the myargs array
+
+  # each time through the loop, shift the arguments left
+  # this decrements the argument count for us
+  shift
+  # tell the user we shifted things
+  echo "Shifted command line, leaving $# things left to process."
+  echo "--------------------------"
+  # go back to the top of the loop to see if anything is left to work on
+done
+
+#printing out contents of myargs array
+echo "Printing out array:"
+echo ${myargs[@]}
+echo "Done"
+
+# TASK2: display the settings and myargs contents
+#         Tell the user if vebose mode is on
+#         Tell the user if debug mode is on and if it is, what number it is set to
+#         Print out the myargs array with a label
   	echo "Added \'$3\' to the arguments array"
   	myargs+=("$3")
   	#shifting twice to exclude both -d and N
